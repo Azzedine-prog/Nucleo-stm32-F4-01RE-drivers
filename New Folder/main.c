1b872c47a95bb06ad3ba782e8ca8a5bc6dc9c039
Azzedine-prog <aazzedinelakhdar@gmail.com>
2021-08-11 15:30:01 +0100

    updated main test

--- New Folder/main.c
+++ New Folder/main.c
@@ -13,22 +13,44 @@
 void set_clock_to_75Mhz(void);
 //void delay(uint32_t seconds);
 void set_clock_to_75Mhz(void){
+	//making sure PLL is disabled
+	RCC->CR &= (uint32_t)~(1<<24);
+	
+	//enabling internal clock source
 	RCC->CR |= 1;
+	
+	//waiting for it to be ready
 	while(!(RCC->CR & 0x2));
-	RCC->CR |= (uint32_t)~(1<<24);
-	RCC->PLLCFGR &= (uint32_t) ~(1<<22);  
+	
+	//making HSI PLL clock_source
+	RCC->PLLCFGR &= (uint32_t) ~(1<<22);
+	
+	//setting n = 12c,m = 10,p = 4 for 75
+	
+	//n=0x12c
+	RCC->PLLCFGR |= 0x12c<<6;
+	//RCC->PLLCFGR &=(uint32_t) ~(0x12c<<6);
+	
+	//m=0x10
+	RCC->PLLCFGR |= 0x10;
+	//RCC->PLLCFGR  &=(uint32_t) ~(0x10);
+	
+	//p=0x4
+	RCC->PLLCFGR |= 0x4<<16;
+	//RCC->PLLCFGR &=(uint32_t) ~(0x4<<16);
+	
+	//enabling PLL
 	RCC->CR |= 1<<24;
-	//n = 12c,m = 10,p = 4 for 75
-		RCC->PLLCFGR |= 0xf0<<6;
-		RCC->PLLCFGR &=(uint32_t) ~(0xf0<<6);
-	  RCC->PLLCFGR |= 0x10;
-		RCC->PLLCFGR  &=(uint32_t) ~(0x10);
-	  RCC->PLLCFGR |= 0x8<<16;
-		RCC->PLLCFGR &=(uint32_t) ~(0x8<<16);
 	while(!((RCC->CR) & (1<<25)));
+	
+	//waiting for PLL to stabilise
+	//RCC->PLLCFGR &= (uint32_t) ~(1<<22);
+	RCC->CFGR |= 0x5<<13;
+	RCC->CFGR |= 0x5<<10;
+	
 }
 int main(){
-	//set_clock_to_75Mhz();
+	set_clock_to_75Mhz();
 	RCC->AHB1ENR |= ((uint32_t)11111) ;//activation of clock of gpios
 	GPIOA->MODER |= 1<<10;             //General purpose output of pin 5 in port A
 	volatile uint32_t led = 1<<5;      // our value of the led
